6b3459bc8a0dc349ddd7df6ded62cc18f9153de7
tampq113 <inetbumbaby123@gmail.com>
2023-11-29 04:51:59 +0700

    Commit Admin

---
+++ media/admin/Cmt/comment.php
@@ -0,0 +1,59 @@
+<div class="full_container">
+
+
+   <div id="content">
+
+      <div class="midde_cont">
+         <div class="container-fluid">
+            <div class="row column_title">
+               <div class="col-md-12">
+                  <div class="page_title">
+                     <h2>Quản Lý Comment</h2>
+                  </div>
+               </div>
+            </div>
+
+            <div class="row column2 graph margin_bottom_30">
+               <div class="col-md-l2 col-lg-12">
+                  <div class="col-md-12">
+                     <div class="white_shd full margin_bottom_30">
+                        <div class="full graph_head">
+                           <div class="heading1 margin_0">
+                              <h2>Quản Lý Comment</h2>
+                           </div>
+                        </div>
+                        <div class="table_section padding_infor_info">
+                           <div class="table-responsive-sm">
+                              <table class="table">
+                                 <thead>
+                                    <tr>
+                                       <th class="font-weight-bold">#</th>
+                                       <th class="font-weight-bold">Products</th>
+                                       <th class="font-weight-bold">Count</th>
+                                       <th class="font-weight-bold">Time</th>
+                                       <th></th>
+                                    </tr>
+                                 </thead>
+                                 <tbody>
+                                    <tr>
+                                       <td>1</td>
+                                       <td></td>
+                                       <td></td>
+                                       <td></td>
+                                       <td>
+                                       <a href="index.php?act=Ct_cmt"><button type="button" class="btn btn-info">Chi Tiết</button></a>
+                                       </td>
+                                    </tr>
+                                 </tbody>
+                              </table>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>        
+      </div>
+   </div>
+</div>
+</div>
---
+++ media/admin/User/unlock.php
@@ -0,0 +1,9 @@
+<?php
+ob_start();
+   $user_id =  $_GET['id'];
+  $db = new User();
+  $loked = $db->unbandUser($user_id);
+  header("Location: index.php?act=black");
+  
+
+?>
